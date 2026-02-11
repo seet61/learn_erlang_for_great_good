0a40bf795f93d478c0bd0db4163e1343eb64dbe5
dmitriy.arefev <dmitriy.arefev@restream.ru>
2026-02-11 17:36:27 +0300

    example with functions.

--- chapter11/kitchen.erl
+++ chapter11/kitchen.erl
@@ -1,7 +1,10 @@
 -module(kitchen).
 -export([
     fridge1/0,
-    fridge2/1
+    fridge2/1,
+    start/1,
+    store/2,
+    take/2
 ]).
 
 fridge1() ->
@@ -29,9 +32,24 @@ fridge2(FoodList) ->
                     From ! {self(), {ok, Food}},
                     fridge2(lists:delete(Food, FoodList));
                 false ->
-                    From ! {self(), {ok, not_found}},
+                    From ! {self(), not_found},
                     fridge2(FoodList)
             end;
     terminate ->
         ok
+    end.
+
+start(FoodList) ->
+    spawn(?MODULE, fridge2, [FoodList]).
+
+store(Pid, Food) ->
+    Pid ! {self(), {store, Food}},
+    receive
+        {Pid, Msg} -> Msg
+    end.
+
+take(Pid, Food) ->
+    Pid ! {self(), {take, Food}},
+    receive
+        {Pid, Msg} -> Msg
     end.
