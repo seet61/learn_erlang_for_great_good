9bc7854b37e8904e38803f7cefe0d41356eeb8da
dmitriy.arefev <dmitriy.arefev@restream.ru>
2026-03-24 15:19:03 +0300

    end of example

--- chapter17/test.erl
+++ chapter17/test.erl
@@ -4,7 +4,9 @@
     test2/0,
     test3/0,
     test4/0,
-    test5/0
+    test5/0,
+    test6/0,
+    test7/0
 ]).
 
 test1() ->
@@ -25,3 +27,21 @@ test4() ->
 
 test5() ->
     band_supervisor:start_link(jerk).
+
+test6() ->
+    band_supervisor:start_link(lenient),
+    io:format("which_children: ~tp ~n", [supervisor:which_children(band_supervisor)]),
+    supervisor:terminate_child(band_supervisor, drum),
+    supervisor:terminate_child(band_supervisor, singer),
+    supervisor:restart_child(band_supervisor, singer),
+    io:format("count_children: ~tp ~n", [supervisor:count_children(band_supervisor)]),
+    supervisor:delete_child(band_supervisor, drum),
+    io:format("restart_child drum after delete: ~tp ~n", [supervisor:restart_child(band_supervisor, drum)]),
+    io:format("count_children: ~tp ~n", [supervisor:count_children(band_supervisor)])
+    .
+
+test7() ->
+    band_supervisor:start_link(jamband),
+    supervisor:start_child(band_supervisor, [djembe, good]),
+    supervisor:start_child(band_supervisor, [djembe, good]),
+    supervisor:delete_child(band_supervisor, drum).
