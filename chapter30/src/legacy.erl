6605b2fdd74e4d4ee860a97da2d365f4b5cad12f
dmitriy.arefev <dmitriy.arefev@restream.ru>
2026-05-05 09:49:01 +0300

    Legacy sha example with different variants

--- chapter30/src/legacy.erl
+++ chapter30/src/legacy.erl
@@ -8,6 +8,7 @@ hash_password(Password, Salt) ->
     PwdBin = unicode:characters_to_binary(Password),
     SaltBin = unicode:characters_to_binary(Salt),
     Combined = <<SaltBin/binary, PwdBin/binary>>,
+    %Combined = <<PwdBin/binary, SaltBin/binary>>,
     Hash = crypto:hash(sha, Combined),
     binary:encode_hex(Hash, lowercase).
 
